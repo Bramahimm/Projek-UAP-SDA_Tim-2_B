e401700413fff7cd33d2f088f042cb095d5adbba
Bramahimm <149980338+Bramahimm@users.noreply.github.com>
2024-06-17 17:26:14 +0700

    Update MauDiet.py

--- MauDiet.py
+++ MauDiet.py
@@ -7,25 +7,25 @@ class Food:
         self.berat = berat
 
     def __repr__(self):
-        return f"Nama Makanan: {self.nama}\nJumlah Kalori: {self.kalori} kcal\nBerat Makanan: {self.berat} gram"
+        return f">Nama Makanan: {self.nama}\nJumlah Kalori: {self.kalori} kkal\nBerat Makanan: {self.berat} gram\n"
 
 class CalorieManager:
     def __init__(self):
         self.daftar = []
 
-    def add_food(self, nama, kalori, berat):
+    def tambahMakanan(self, nama, kalori, berat):
         new_food = Food(nama, kalori, berat)
         self.daftar.append(new_food)
-        print(f"Ditambahkan:\n {new_food}")
+        print(f"\n\nDitambahkan:\n{new_food}")
 
-    def view_foods(self):
+    def lihatMakanan(self):
         if not self.daftar:
             print("Makanan tidak ditemukan dalam list.")
         else:
             for food in self.daftar:
                 print(food)
 
-    def update_food(self, nama, kalori_baru, berat_baru):
+    def perbarui(self, nama, kalori_baru, berat_baru):
         for food in self.daftar:
             if food.nama == nama:
                 food.kalori = kalori_baru
@@ -34,7 +34,7 @@ class CalorieManager:
                 return
         print("Makanan tidak ditemukan.")
 
-    def delete_food(self, nama):
+    def hapusMakanan(self, nama):
         for food in self.daftar:
             if food.nama == nama:
                 self.daftar.remove(food)
@@ -42,17 +42,27 @@ class CalorieManager:
                 return
         print("Makanan tidak ditemukan.")
 
-    def search_food(self, nama):
+    def cariMakanan(self, nama):
         for food in self.daftar:
             if food.nama == nama:
                 return food
         return None
 
-    def sort_foods_by_calories(self):
-        self.daftar.sort(key=lambda food: food.kalori)
-        print("Makanan diurutkan berdasarkan kalori.")
+    def sortingMakanan(self):
+        angka = len(self.daftar)
+        if angka == 0:
+            print("Tidak ada makanan untuk diurutkan.")
+            return
+        
+        for i in range(angka):
+            for j in range(0, angka-i-1):
+                if self.daftar[j].kalori > self.daftar[j+1].kalori:
+                    self.daftar[j], self.daftar[j+1] = self.daftar[j+1], self.daftar[j]
+        
+        print("\n\nBerikut adalaah Makananmu yang udah diurutin berdasarkan kalori :")
+        self.lihatMakanan()
 
-    def export_to_csv(self, nama_file):
+    def exporKecsv(self, nama_file):
         with open(nama_file, mode='w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(["Nama Makanan", "Kalorinya", "Berat"])
@@ -60,7 +70,7 @@ class CalorieManager:
                 writer.writerow([food.nama, food.kalori, food.berat])
         print(f"Data berhasil diexport dengan nama {nama_file}")
 
-    def import_from_csv(self, path_file):
+    def imporDaricsv(self, path_file):
         try:
             with open(path_file, mode='r') as file:
                 reader = csv.reader(file)
@@ -79,49 +89,65 @@ def main():
     manager = CalorieManager()
 
     while True:
-        print("\nCalorie Manager")
-        print("1. Tambah Makanan")
-        print("2. Lihat Makanan")
-        print("3. Perbarui Makanan")
-        print("4. Hapus Makanan")
-        print("5. Cari Makanan")
-        print("6. Urutin Makanan berdasarkan Kalori")
-        print("7. Export ke CSV")
-        print("8. Import dari CSV")
-        print("9. Exit")
-        
+        print("========================================")
+        print("||          <Calorie Manager>         ||")
+        print("||>>>>>>>>>>>>>>>>>||<<<<<<<<<<<<<<<<<||")
+        print("||1. Tambah Makanan                   ||")
+        print("||2. Lihat Makanan                    ||")
+        print("||3. Perbarui Makanan                 ||")
+        print("||4. Hapus Makanan                    ||")
+        print("||5. Cari Makanan                     ||")
+        print("||6. Urutin Makanan berdasarkan Kalori||")
+        print("||7. Export ke CSV                    ||")
+        print("||8. Import dari CSV                  ||")
+        print("||9. Keluar                           ||")
+        print("========================================")
         pilihan = input("Masukkan pilihan: ")
 
         if pilihan == '1':
-            nama = input("Masukkan nama makanan: ")
-            kalori = int(input("Jumlah kalori: "))
-            berat = int(input("Masukkan berat makanan (gram): "))
-            manager.add_food(nama, kalori, berat)
+            penentu = True
+            while penentu:
+                try:
+                    nama = input("Masukkan nama makanan: ")
+                    kalori = int(input("Jumlah kalori: "))
+                    berat = int(input("Masukkan berat makanan (gram): "))
+                    manager.add_food(nama, kalori, berat)
+                except ValueError:
+                    print("\n\n>>> Input tidak valid. Kembali ke menu utama <<<\n\n")
+                    penentu = False
+                continue
+            
         elif pilihan == '2':
-            manager.view_foods()
+            manager.lihatMakanan()
+            
         elif pilihan == '3':
             nama = input("Nama makanan yang akan diedit: ")
             kalori_baru = int(input("Jumlah kalori baru: "))
             berat_baru = int(input("Berat baru (gram): "))
-            manager.update_food(nama, kalori_baru, berat_baru)
+            manager.perbarui(nama, kalori_baru, berat_baru)
+            
         elif pilihan == '4':
             nama = input("Nama makanan yang akan dihapus: ")
-            manager.delete_food(nama)
+            manager.hapusMakanan(nama)
+            
         elif pilihan == '5':
             nama = input("Nama makanan yang akan dicari: ")
-            food = manager.search_food(nama)
+            food = manager.cariMakanan(nama)
             if food:
                 print(f"Ketemu: {food}")
             else:
                 print("Makanan tidak ditemukan.")
         elif pilihan == '6':
-            manager.sort_foods_by_calories()
+            manager.sortingMakanan()
+            
         elif pilihan == '7':
             nama_file = input("Masukkan nama file untuk diexport: ")
-            manager.export_to_csv(nama_file)
+            manager.exporKecsv(nama_file)
+            
         elif pilihan == '8':
             path_file = input("Masukkan path file untuk diimport: ")
-            manager.import_from_csv(path_file)
+            manager.imporDaricsv(path_file)
+            
         elif pilihan == '9':
             break
         else:
